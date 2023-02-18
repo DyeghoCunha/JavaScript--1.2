69f63651e023231e19d192ba34eadf10638221b6
Dyegho <dyeghocunha@gmail.com>
2023-02-17 23:29:01 -0300

    finalizando o dia

---
+++ Tipos, variáveis, funções/java.js
@@ -0,0 +1,48 @@
+
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+
+//console.log(operacaoMatematica)
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+
+const pontoFlutuante = 0.5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+//console.log(novaOperacao)
+
+
+
+// NaN -> Not a Number
+
+const alura = "Alura";
+
+
+//String
+
+const texto1 = "Ola,Mundo"
+
+const texto = 'Ola,Mundo'
+
+const senha = "senhaSuperSegura456!"
+
+const stringDeNumeros = "34567"
+
+const citacao = 'O Leo disse "Oi"'
+
+console.log(citacao)
+
+//template string ou template literal
+
+ 
